bd878968b505f3990af4fbfdda1fbeb8820a54e3
Jezeh Priesten <jepriesten@gmail.com>
2019-09-19 23:00:41 +0100

    Modified server template to accomodate admin view login to pohpoh

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,25 +1,25 @@
-module.exports = (app) => {
-    const users = require('../controllers/user.controller.js');
-    const authenticate = require('../middlewares/authenticator.middleware').default;
+module.exports = app => {
+  const users = require("../controllers/user.controller.js");
+  const authenticate = require("../middlewares/authenticator.middleware");
 
-    // Create a new User
-    app.post('/users/create', users.create);
+  // Create a new User
+  app.post("/users/create", users.create);
 
-    // Login a user
-    app.post('/users/login', users.login);
+  // Login a user
+  app.post("/users/login", users.login);
 
-    // Login a user out
-    app.post('/users/logout', authenticate, users.logout);
+  // Login a user out
+  app.post("/users/logout", authenticate, users.logout);
 
-    // Retrieve all Users
-    app.get('/users', authenticate, users.findAll);
+  // Retrieve all Users
+  app.get("/users", authenticate, users.findAll);
 
-    // Retrieve a single User with userId
-    app.get('/users/me', authenticate, users.findOne);
+  // Retrieve a single User with userId
+  app.get("/users/me", authenticate, users.findOne);
 
-    // Update aUser with userId
-    app.put('/users/:userId', users.update);
+  // Update aUser with userId
+  app.put("/users/:userId", users.update);
 
-    // Delete a User with userId
-    app.delete('/users/:userId', users.delete);
-}
+  // Delete a User with userId
+  app.delete("/users/:userId", users.delete);
+};
